5cd64353ca205222009af5e0c31c58cd88419a34
WeifuLi <40319294+WeifuLi@users.noreply.github.com>
2019-03-07 23:55:17 +0800

    Update Readme.m

--- Readme.m
+++ Readme.m
@@ -1,6 +1,7 @@
 % Three geographical regions including Libya, Arabian Peninsula and Australian are chosen for geolocation error estimation.
-% 11, 19, 24 MWRI data across these regions are adopted from Jan. 1 to Feb. 28 in 2016, respectively.
+% 11, 19, 24 MWRI data across these regions are adopted from Jan. 1 to Feb. 28 in 2016, respectively. The chosen data can be 
+% from the excel file and download from the FY Satellite Data Center.
 
-% Please run the functon main.m to see the estimated geolocation error. 
+% After downloading the data, please run the functon main.m to see the estimated geolocation error. 
 % The detailed description can be found in paper "Improved Estimation of Geolocation Errors in MWRI Data Aboard FY Series Satellites".
 % If you have any problem, please contact the email wfli@stu.hubu.edu.cn 
